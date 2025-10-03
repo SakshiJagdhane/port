64b0866df893f2fc3bd457eb719d0c5f9e90565a
Sakshi Jagdhane <133689114+SakshiJagdhane@users.noreply.github.com>
2025-10-03 23:55:09 +0530

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,9 +2,9 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import wanderlust from "../../Assets/Projects/wanderlust.png";
-import elderlycare from "../../Assets/Projects/elderlycare.png";
-import docappointment from "../../Assets/Projects/docappointment.png";
+import leaf from "../../Assets/Projects/leaf.png";
+import editor from "../../Assets/Projects/editor.png";
+import chatify from "../../Assets/Projects/chatify.png";
 import blog from "../../Assets/Projects/blog.png";
 
 function Projects() {
@@ -21,7 +21,7 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={blog}
+              imgPath={chatify}
               isBlog={false}
               title="Full Stack Doctor Appointment Website"
               description="Responsive booking platform enabling patients to schedule appointments and doctors to
@@ -33,7 +33,7 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={wanderlust}
+              imgPath={blog}
               isBlog={false}
               title="NearMeExplorers"
               description="NearMe Explorer is a location-based web that helps users discover nearby places, services, and attractions in real-time."
@@ -44,7 +44,7 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={elderlycare}
+              imgPath={editor}
               isBlog={false}
               title="Elderly_Care_Website"
               description="An Elderly Care website provides information, services, and resources to support the health, safety, and well-being of senior citizens and their caregivers"
@@ -55,7 +55,7 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={wanderlust}
+              imgPath={leaf}
               isBlog={false}
               title="WanderLust"
               description="Wanderlust is a travel-focused platform that inspires, guides, and helps users plan trips, explore destinations, and share travel experiences"
