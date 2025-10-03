7049f146b7f52696443ff43aa0a7bd735bfaf736
Sakshi Jagdhane <133689114+SakshiJagdhane@users.noreply.github.com>
2025-10-03 22:47:10 +0530

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,12 +2,10 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+import wanderlust from "../../Assets/Projects/wanderlust.png";
+import elderlycare from "../../Assets/Projects/elderlycare.png";
+import docappointment from "../../Assets/Projects/docappointment.png";
+import blog from "../../Assets/Projects/blog.png";
 
 function Projects() {
   return (
@@ -23,7 +21,7 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={docappointment}
               isBlog={false}
               title="Full Stack Doctor Appointment Website"
               description="Responsive booking platform enabling patients to schedule appointments and doctors to
@@ -35,7 +33,7 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={blog}
               isBlog={false}
               title="NearMeExplorers"
               description="NearMe Explorer is a location-based web that helps users discover nearby places, services, and attractions in real-time."
@@ -46,7 +44,7 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={elderlycare}
               isBlog={false}
               title="Elderly_Care_Website"
               description="An Elderly Care website provides information, services, and resources to support the health, safety, and well-being of senior citizens and their caregivers"
@@ -57,7 +55,7 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={leaf}
+              imgPath={wanderlust}
               isBlog={false}
               title="WanderLust"
               description="Wanderlust is a travel-focused platform that inspires, guides, and helps users plan trips, explore destinations, and share travel experiences"
@@ -66,28 +64,7 @@ function Projects() {
             />
           </Col>
 
-          {/* <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={suicide}
-              isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
-              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={emotion}
-              isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
-            />
-          </Col> */}
+         
         </Row>
       </Container>
     </Container>
